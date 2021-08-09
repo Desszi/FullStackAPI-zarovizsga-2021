1792f6a234e8b944b00eaf3dee22cd9fd16d986a
Mercédesz Anett Porkoláb <09dporkolabm@mail.com>
2021-08-09 14:35:23 +0200

    Első feladat kész

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,43 @@
+const mongoose = require('mongoose');
+
 // 1. Feladat: pótold a service modellt.
+// export class Service {
+//     _id: string = '';
+//     name: string = '';
+//     since: number = 0;
+//     street?: string = '';
+//     city?: string = '';
+//     country?: string = '';
+//     active?: boolean = true;
+//   }
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        required: true
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timeStamps: true
+});
+
+module.exports = mongoose.model('service', ServiceSchema);
